36ac78b7ef8a5f58a2f6d96af7502864d6a41dc0
nmburgan <13688219+nmburgan@users.noreply.github.com>
2025-12-30 17:53:51 -0800

    Set versions to releases and update deploy repo

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 
   :min-lein-version "2.7.1"
 
-  :parent-project {:coords [org.openvoxproject/clj-parent "7.4.1-SNAPSHOT"]
+  :parent-project {:coords [org.openvoxproject/clj-parent "7.5.0"]
                    :inherit [:managed-dependencies]}
 
   ;; Abort when version ranges or version conflicts are detected in
@@ -51,12 +51,12 @@
   ;; this plugin is used by jenkins jobs to interrogate the project version
   :plugins [[lein-parent "0.3.9"]
             [jonase/eastwood "1.2.2" :exclusions [org.clojure/clojure]]
-            [org.openvoxproject/i18n "0.9.3-SNAPSHOT"]]
+            [org.openvoxproject/i18n "0.9.3"]]
 
   :lein-release        {:scm          :git
                         :deploy-via   :lein-deploy}
 
-  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/CLOJARS_USERNAME
                                      :password :env/CLOJARS_PASSWORD
                                      :sign-releases false}]]
